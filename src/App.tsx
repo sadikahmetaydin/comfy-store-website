b31fd5a4cc71cf7db255edf3ccbdba9222ff5382
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-02-03 21:30:16 +0300

    install tailwindcss, shadcn/ui, redux toolkit and define all structures

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-function App() {
+import { Button } from "@/components/ui/button";
+import { useAppSelector } from "./hooks";
+
+export default function App() {
+  const { name } = useAppSelector((state) => state.userState);
+  console.log(name);
+
   return (
     <div>
-      <h1>sadikahmetaydin! || helloWorld!</h1>
+      <h1 className="text-white bg-blue-800">helloWorld!</h1>
+
+      <Button
+        variant="destructive"
+        size="lg"
+        onClick={() => console.log("it worked!!!")}
+      >
+        Click me
+      </Button>
     </div>
-  )
+  );
 }
-export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+import { Provider } from "react-redux";
+import { store } from "./store.ts";
+
+createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
     <App />
-  </StrictMode>,
-)
+  </Provider>
+);
