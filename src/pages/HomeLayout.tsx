7d8913df4c485f77ac138f9b14866323cb613375
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-02-04 21:02:46 +0300

    create header component

--- src/pages/HomeLayout.tsx
+++ src/pages/HomeLayout.tsx
@@ -1,9 +1,10 @@
+import { Header } from "@/components";
 import { Outlet } from "react-router-dom";
 
 function HomeLayout() {
   return (
     <>
-      <header>header</header>
+      <Header />
       <nav>navbar</nav>
       <Outlet />
     </>
