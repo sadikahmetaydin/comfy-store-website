11cc713d4805077e44d37b5397a0565f5761e9cd
sadikahmetaydin <sadikahmetaydin@gmail.com>
2025-02-06 19:33:51 +0300

    About Page Component

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,23 @@
 function About() {
-  return <h1 className="text-4xl">About Page</h1>;
+  return (
+    <section>
+      <h1 className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center text-4xl font-bold leading-none tracking-wide sm:text-6xl">
+        We love{" "}
+        <span className="bg-primary py-2 px-4 rounded-lg tracking-widest text-white">
+          comfy
+        </span>
+      </h1>
+
+      <p className="mt-6 text-lg tracking-wide leading-8 max-w-2xl mx-auto">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam facere
+        accusantium iure saepe magni praesentium ipsam enim obcaecati quae est
+        dolorem asperiores repudiandae dolorum officia modi corporis dolor natus
+        sunt beatae, distinctio amet neque possimus. Iste aliquid quod magnam
+        accusamus adipisci quis temporibus consequuntur, ipsam hic molestias
+        optio nesciunt, amet ratione similique, quasi perspiciatis totam!
+      </p>
+    </section>
+  );
 }
 
 export default About;
